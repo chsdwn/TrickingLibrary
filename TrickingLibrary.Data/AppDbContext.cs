6e9e4faa43bfde639313836e08e0da3162c2eb43
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-10-26 11:33:29 +0300

    10. Trick Data

--- TrickingLibrary.Data/AppDbContext.cs
+++ TrickingLibrary.Data/AppDbContext.cs
@@ -8,7 +8,32 @@ namespace TrickingLibrary.Data
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TrickCategory>()
+                        .HasKey(tc => new { tc.CategoryId, tc.TrickId });
+
+            modelBuilder.Entity<TrickRelationship>()
+                        .HasKey(tr => new { tr.PrerequisiteId, tr.ProgressionId });
+
+            modelBuilder.Entity<TrickRelationship>()
+                        .HasOne(tr => tr.Prerequisite)
+                        .WithMany(t => t.Progressions)
+                        .HasForeignKey(tr => tr.PrerequisiteId);
+
+            modelBuilder.Entity<TrickRelationship>()
+                        .HasOne(tr => tr.Progression)
+                        .WithMany(t => t.Prerequisites)
+                        .HasForeignKey(tr => tr.ProgressionId);
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
         public DbSet<Submission> Submissions { get; set; }
         public DbSet<Trick> Tricks { get; set; }
+        public DbSet<TrickCategory> TrickCategories { get; set; }
+        public DbSet<TrickRelationship> TrickRelationships { get; set; }
     }
 }
--- TrickingLibrary.Models/Trick.cs
+++ TrickingLibrary.Models/Trick.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+
 namespace TrickingLibrary.Models
 {
     public class Trick : BaseModel<string>
     {
         public string Name { get; set; }
+        public string Description { get; set; }
+        public string DifficultyId { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public IList<TrickRelationship> Prerequisites { get; set; }
+        public IList<TrickRelationship> Progressions { get; set; }
+        public IList<TrickCategory> TrickCategories { get; set; }
     }
 }
